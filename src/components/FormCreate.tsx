d007de55cf053d7aed16a04fb6dc08b62ca703c9
alexmm23 <alejandromontes393@gmail.com>
2024-06-08 15:07:17 -0600

    Refactorizacion de formulario

---
+++ src/components/FormCreate.tsx
@@ -0,0 +1,37 @@
+import { GenericElementProps, StyleProps } from "../types/General";
+import { postData } from "../utils/functions";
+import { validateForm } from "../utils/functions";
+import { useState } from "react";
+
+function FormCreate({ children, className }: GenericElementProps & StyleProps) {
+  const [error, setError] = useState<boolean>(false);
+  const handleOnSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setError(false);
+    //Get all the data from children in the form
+    const form = document.querySelector("form") as HTMLFormElement;
+    const entries = new FormData(form).entries() as any;
+    const data = Object.fromEntries(entries);
+    const isValid = validateForm(data);
+    if (!isValid) {
+      setError(true);
+      return;
+    }
+    //Send the data to the server
+    postData(data);
+  };
+  return (
+    <form method="POST" onSubmit={handleOnSubmit} className={className}>
+      {error && (
+        <p className="invalid-feedback text-center">
+          Todos los campos son requeridos
+        </p>
+      )}
+      {children}
+      <button className="mb-20 w-100 btn-submit" type="submit">
+        Enviar
+      </button>
+    </form>
+  );
+}
+export default FormCreate;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,9 +5,15 @@ import DashboardLayout from "../layout/DashboardLayout";
 import ProductCard from "../components/ProductCard";
 import { getTokenFromLocalStorage } from "../utils/functions";
 import { useNavigate } from "react-router-dom";
+import Modal from "../components/Modal";
+import Input from "../components/Input";
+import FormCreate from "../components/FormCreate";
+import { handleShowModal, handleCloseModal } from "../utils/functions";
+import Select from "../components/Select";
 function Products() {
   const [products, setProducts] = useState<Array<Object>>([]);
   const token = getTokenFromLocalStorage();
+
   const navigate = useNavigate();
   const fetchProducts = async () => {
     const response = await fetch(`${URL_API}/v1/products`, {
@@ -32,6 +38,9 @@ function Products() {
     <DashboardLayout>
       <section className="section-center">
         <Header title="Products" />
+        <section>
+          <button onClick={handleShowModal}>Nuevo</button>
+        </section>
         <section className="row">
           {products.map((product: any) => (
             <ProductCard
@@ -42,6 +51,73 @@ function Products() {
             />
           ))}
         </section>
+        <Modal onClick={handleCloseModal}>
+          <header>
+            <h2 className="text-center">Agregar producto</h2>
+          </header>
+          <FormCreate className="">
+            <div className="d-flex flex-column">
+              <section>
+                <Input
+                  type="text"
+                  placeholder="Nombre del producto"
+                  className="input-text flex-wrap-row"
+                  name="name"
+                  label="Nombre"
+                />
+              </section>
+              <section>
+                <Input
+                  type="text"
+                  placeholder="Descripcion del producto"
+                  className="input-text"
+                  name="description"
+                  label="Descripcion"
+                />
+              </section>
+              <section>
+                <Select
+                  options={[{ label: "prueba", value: "0" }]}
+                  value="-"
+                  label="Categoria"
+                  name="category"
+                />
+              </section>
+              <section className="d-flex gap-10">
+                <Input
+                  type="currency"
+                  placeholder="20.00 mxn"
+                  className="input-number"
+                  name="unitCost"
+                  label="Costo unitario"
+                />
+                <Input
+                  type="currency"
+                  placeholder="20.00 mxn"
+                  className="input-number"
+                  name="unitPrice"
+                  label="Precio unitario"
+                />
+              </section>
+              <section className="d-flex gap-10">
+                <Input
+                  type="number"
+                  placeholder="0"
+                  className="input-number"
+                  name="quantity"
+                  label="Cantidad"
+                />
+                <Input
+                  type="text"
+                  placeholder="pcs, kg, lts"
+                  className="input-text"
+                  name="measureUnit"
+                  label="Unidad de medida"
+                />
+              </section>
+            </div>
+          </FormCreate>
+        </Modal>
       </section>
     </DashboardLayout>
   );
