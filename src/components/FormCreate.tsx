aeb94d2eea3a590d81941b3d07082d0811567b2e
alexmm23 <alejandromontes393@gmail.com>
2024-06-08 20:12:17 -0600

    Limpieza de codigo

--- src/components/FormCreate.tsx
+++ src/components/FormCreate.tsx
@@ -1,38 +1,12 @@
-import { GenericElementProps, StyleProps } from "../types/General";
-import { postData } from "../utils/functions";
-import { validateForm } from "../utils/functions";
-import { useState } from "react";
+import { FormProps, GenericElementProps, StyleProps } from "../types/General";
 
-function FormCreate({ children, className }: GenericElementProps & StyleProps) {
-  const [error, setError] = useState<boolean>(false);
-  const handleOnSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    setError(false);
-    //Get all the data from children in the form
-    const form = document.querySelector("form") as HTMLFormElement;
-    const entries = new FormData(form).entries() as any;
-    const data = Object.fromEntries(entries);
-    const isValid = validateForm(data);
-    if (!isValid) {
-      setError(true);
-      return;
-    }
-    //Send the data to the server
-    const response = await postData(data, "products");
-    if (response.status === 200) {
-      //Render all the products updated
-      window.location.reload();
-    } else {
-      setError(true);
-    }
-  };
+function FormCreate({
+  children,
+  className,
+  onSubmit,
+}: GenericElementProps & StyleProps & FormProps) {
   return (
-    <form method="POST" onSubmit={handleOnSubmit} className={className}>
-      {error && (
-        <p className="invalid-feedback text-center">
-          Todos los campos son requeridos
-        </p>
-      )}
+    <form method="POST" onSubmit={onSubmit} className={className}>
       {children}
       <button className="mb-20 w-100 btn-submit" type="submit">
         Enviar
